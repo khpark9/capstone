f02d51ef862afc9b9bd78bac0c4579df0d58ef10
khpark9 <105660250+khpark9@users.noreply.github.com>
2024-11-18 03:33:24 -0600

    edits

--- docs/q1/sketch.js
+++ docs/q1/sketch.js
@@ -133,14 +133,14 @@ export default function define(runtime, observer) {
     ["movies.csv", {url: new URL("./data/movies.csv", import.meta.url), mimeType: "text/csv", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
-  main.variable(observer()).define(["md"], _1);
+  // main.variable(observer()).define(["md"], _1);
   main.variable(observer("viewof order")).define("viewof order", ["Inputs"], _order);
   main.variable(observer("order")).define("order", ["Generators", "viewof order"], (G, _) => G.input(_));
   main.variable(observer("chart")).define("chart", ["d3","data"], _chart);
   main.variable(observer("update")).define("update", ["chart","order"], _update);
   main.variable(observer("data")).define("data", ["FileAttachment"], _data);
-  main.variable(observer()).define(["md"], _6);
+  // main.variable(observer()).define(["md"], _6);
   main.variable(observer("trigger")).define("trigger", ["viewof order","d3","Event","invalidation"], _trigger);
-  main.variable(observer()).define(["md"], _8);
+  // main.variable(observer()).define(["md"], _8);
   return main;
 }
